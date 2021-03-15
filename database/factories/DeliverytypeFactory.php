386039eb176ec6b89aa9b319102e27e7c17f41ba
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-03-15 12:01:33 -0300

    Carga nuevas migraciones

---
+++ database/factories/DeliverytypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Deliverytype;
+use Faker\Generator as Faker;
+
+$factory->define(Deliverytype::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
