b9c5dd48706e8b7cc14828eff7617fe088a82377
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-03-15 10:03:29 -0300

    Añade modelos

---
+++ database/factories/MeasureunitFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Measureunit;
+use Faker\Generator as Faker;
+
+$factory->define(Measureunit::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
