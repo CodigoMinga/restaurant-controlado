f77630752b934093c335bfef76bb3b2bf2cda1ed
Roberto Sierra <roberto30589@gmail.com>
2021-03-16 22:03:04 -0300

    search y elimina svg

--- resources/views/templates/maincontainer.blade.php
+++ resources/views/templates/maincontainer.blade.php
@@ -41,7 +41,7 @@
     <div id="main">
         <div class="d-flex flex-column h-100">
             <div class="p-4">
-                <h1><i class="material-icons" style="font-size:2.51rem;vertical-align:-0.5rem">bookmarks</i> Categorias</h1>
+                <h1><i class="material-icons">bookmarks</i> Categorias</h1>
             </div>
             <div class="d-flex flex-row justify-content-around flex-wrap p-4" id="categoria-container">
                 @for ($i = 0; $i < 50; $i++)
@@ -50,8 +50,12 @@
                 </div>
                 @endfor
             </div>
-            <div class="d-flex flex-row justify-content-between p-4">
-                <h1><i class="material-icons" style="font-size:2.51rem;vertical-align:-0.5rem">fastfood</i> Productos</h1>
+            <div class="d-flex flex-row justify-content-between p-4 flex-wrap">
+                <h1><i class="material-icons">fastfood</i> Productos</h1>
+                <div class="cm-form-icon">
+                    <input type="text">
+                    <i class="material-icons">search</i>
+                </div>
             </div>
             <div class="d-flex flex-row justify-content-around flex-wrap p-4" id="producto-container">
                 @for ($i = 0; $i < 50; $i++)
