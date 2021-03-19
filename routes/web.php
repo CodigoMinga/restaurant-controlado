d13dcbfcb0d6a848baaf7c7eacd3c2ee53c0de47
Sebastian <sebastian.vera.moll@gmail.com>
2021-03-19 11:18:38 -0300

    se arregla ruta login

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('templates.categorias');
 });
+Route::get('/login',function(){
+    return view('login.login');
+});
 
 
 route::get('/app/items/add','ItemController@add');
