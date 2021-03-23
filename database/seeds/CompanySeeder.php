3578c80f17f039452401e8e30f8bfe184822b2ae
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-03-23 11:58:24 -0300

    Finaliza helper para emitir boletas OpenFactura

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -14,11 +14,11 @@ class CompanySeeder extends Seeder
     public function run()
     {
         Company::create(['name'=>'CodigoMinga Bar',
-            'rut'=>'77012555-3',
-            'razon_social'=>'Colaborativa y Cooperativa Conectando',
-            'giro' => 'Restaurant',
-            'direccion' => 'Quellon S/N',
-            'comuna' => 'Quellon',
+            'rut'=>'76795561-8',
+            'razon_social'=>'HAULMER SPA',
+            'giro' => 'VENTA AL POR MENOR EN EMPRESAS DE VENTA A DISTANCIA VÍA INTERNET; COMERCIO ELEC',
+            'direccion' => 'ARTURO PRAT 527   CURICO',
+            'comuna' => 'Curicó',
             'api_key_openfactura' => '928e15a2d14d4a6292345f04960f4bd3'
         ]);
         Company::create(['name'=>'Delixius',
