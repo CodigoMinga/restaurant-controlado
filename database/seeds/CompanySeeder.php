169b84c94330a8f524f21f7b6b601507ac5fb308
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-03-23 10:13:15 -0300

    Añade datos de la empresa al seeder

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -13,7 +13,21 @@ class CompanySeeder extends Seeder
      */
     public function run()
     {
-        Company::create(['name'=>'CodigoMinga Bar']);
-        Company::create(['name'=>'Delixius']);
+        Company::create(['name'=>'CodigoMinga Bar',
+            'rut'=>'77012555-3',
+            'razon_social'=>'Colaborativa y Cooperativa Conectando',
+            'giro' => 'Restaurant',
+            'direccion' => 'Quellon S/N',
+            'comuna' => 'Quellon',
+            'api_key_openfactura' => '928e15a2d14d4a6292345f04960f4bd3'
+        ]);
+        Company::create(['name'=>'Delixius',
+            'rut'=>'77012555-3',
+            'razon_social'=>'DARUICHRODRIGUEZ SPA',
+            'giro' => 'Restaurant',
+            'direccion' => 'Santiago S/N',
+            'comuna' => 'Santiago',
+            'api_key_openfactura' => '928e15a2d14d4a6292345f04960f4bd3'
+        ]);
     }
 }
