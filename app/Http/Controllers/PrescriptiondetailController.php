f465b8b6bd8f19eb383695dd3b6cdc73396820df
Nikotine <nikotine991@gmail.com>
2021-03-31 15:54:29 -0300

    empesando prescriptiondetails

--- app/Http/Controllers/PrescriptiondetailController.php
+++ app/Http/Controllers/PrescriptiondetailController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Prescriptiondetail;
 use Illuminate\Http\Request;
+use App\Prescription;
+use App\Item;
 
 class PrescriptiondetailController extends Controller
 {
