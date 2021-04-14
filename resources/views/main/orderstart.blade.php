1cc1245876ae9ccb9ce15cc2c853dfe73415bf16
Roberto Sierra <roberto30589@gmail.com>
2021-04-14 17:53:00 -0400

    Mejoras visuales, cambio rutas compañias

--- resources/views/main/orderstart.blade.php
+++ resources/views/main/orderstart.blade.php
@@ -1,9 +1,21 @@
 @extends('templates.maincontainer')
 
 @section('content')
-    <div class="container p-3">
-        <h1>Iniciar Orden</h1>
-        <h5>¿Quiere iniciar una Orden en la mesa {{$table->name}}?</h5>
-        <a href="{{url('/orderstart/'.$table->id)}}">Iniciar</a>
+    <div class="container p-3" align="center">
+        <div style="max-width: 500px;"> 
+            <h1>Iniciar Orden</h1>
+            <h5>¿Quiere iniciar una Orden en la mesa {{$table->name}}?</h5>
+            <div class="d-flex  justify-content-between" >
+                <a class="btn btn-success btn-lg" href="{{url('/orderstart/'.$table->id)}}">
+                    <span class="material-icons">receipt_long</span>
+                    Iniciar
+                </a>
+                <button class="btn btn-danger btn-lg" onclick="window.history.back();">
+                    <span class="material-icons">close</span>
+                    Cerrar
+                </button>
+            </div>
+        </div>
+
     </div>
 @stop
