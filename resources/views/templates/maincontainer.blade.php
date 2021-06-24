df09c0f128d1bbd75dd4614b77a6828d3966a48d
Roberto Notebook <roberto30589@gmail.com>
2021-06-24 18:56:01 -0400

    favicon

--- resources/views/templates/maincontainer.blade.php
+++ resources/views/templates/maincontainer.blade.php
@@ -12,6 +12,7 @@ if(!$cashregister){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:image" content="{{url('/img/favicon.png')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="{{url('/css/restorant.css')}}" rel="stylesheet">
